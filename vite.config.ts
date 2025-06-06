842274fc1d627a60b518e2d8182026035bf0b91d
lunech <70895350+LunEcH@users.noreply.github.com>
2025-06-07 00:07:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-//! "my-repo-name" kısmını kendi GitHub reposunun adıyla değiştir!
 export default defineConfig({
-  base: '/my-repo-name/', // Örneğin: /my-react-app/
+  base: '/lunech/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
-    host: true, // Listen on all local IPs
+    host: true,
     port: 5173,
   },
 });
